04188fd9658c851e42cbb7c7f94a4177c5aa0dd7
angelgallardoe <43081306+angelgallardoe@users.noreply.github.com>
2018-09-25 01:51:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,25 @@
+// Import the discord.js module //
 const Discord = require('discord.js');
+
+// Create an instance of a Discord client //
 const client = new Discord.Client();
 
+// The ready event is vital, it means that only _after_ this will your bot start reacting to information received from Discord //
 client.on('ready', () => {
-    console.log('I am ready!');
+console.log('I am ready!');
 });
 
+// Create an event listener for new guild members //
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Bienvenido a la weá, ${member}`);
+});
+
+// M E N S A J E S //
 client.on('message', message => {
     if (message.content === '!ping') {
     	message.reply('Pong!');
@@ -15,5 +30,5 @@ client.on('message', message => {
      }
 });
 
-// THIS  MUST  BE  THIS  WAY
+// THIS  MUST  BE  THIS  WAY //
 client.login(process.env.bot_token);
