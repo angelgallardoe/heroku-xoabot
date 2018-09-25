7b5699b7f9375cd3ceec42dde40b795b0c305285
angelgallardoe <43081306+angelgallardoe@users.noreply.github.com>
2018-09-25 01:28:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,27 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === '!ping') {
-    	message.reply('pong');
-  	}
-});
+client.on('message', message => 
+            {
+    if (message.content === '!ping') 
+    {  	message.reply('pong'); 	}
+            }
+else
+    if (message.content === '!cigarro') 
+    {  	message.reply('No tengo gil conchetumare'); 	}
+            }
+         );
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.bot_token);
+
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  } else
+
+  if (message.content.startsWith("foo")) {
+    message.channel.send("bar!");
+  }
+});
