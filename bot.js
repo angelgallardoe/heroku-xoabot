6d8a2940d4f19ef325ae34f2d5480cebbd2c18cd
angelgallardoe <43081306+angelgallardoe@users.noreply.github.com>
2018-09-28 11:05:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,12 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-// The ready event is vital, it means that only _after_ this will your bot start reacting to information received from Discord //
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-console.log('I am ready!');
+  console.log('I am ready!');
 });
 
 // Create an event listener for new guild members
@@ -19,22 +22,30 @@ client.on('guildMemberAdd', member => {
   channel.send('Welcome to the server, ${member}');
 });
 
-// msg //
-client.on("message", (message){ // EventEmitter
+// PING CHECK//
+client.on("message", (message)){ // EventEmitter
 	if(message.content == "!ping"){ // Check if message is "!ping"
 			message.channel.send("Pinging ...") // Placeholder for pinging ... 
 			.then((msg) => { // Resolve promise
 				msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
-			}
-  }  
-    else if (message.content == "!ping") {
+			});
+		}
+}
+
+// M E N S A J E S //
+client.on("message", (message) => {
+  if (message.content == ("ping")) {
+    message.channel.send("pong!");
+  }
+  else if (message.content == "!ping") {
     	message.reply("Pong!");
   	} 
-        
-else if (message.content == "!cigarro") {
+    else if (message.content == "!cigarro") {
         message.reply("No tengo gil conchetumare");
      }  
 });
+
+
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login('NDkzOTY2MTY1Njg1NTY3NDk0.Dosukg.zXRcfBJRL1j4QBrlZ_UhoP2ICWQ');
