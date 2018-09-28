244760777b20e596eb931f0398c46d5198fdae90
angelgallardoe <43081306+angelgallardoe@users.noreply.github.com>
2018-09-28 14:32:43 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 /** * A bot that welcomes new guild members when they join */
 // Import the discord.js module
 const Discord = require('discord.js');
+const fs = require("fs");
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -95,6 +96,40 @@ client.on('message', message => {
 			message.channel.send('there was an error trying to prune messages in this channel!');
 		});
 	}
+});
+
+//exp//
+
+
+
+let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+const prefix = "+";
+
+client.on("message", message => {
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+
+  if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+    level: 0
+  };
+  let userData = points[message.author.id];
+  userData.points++;
+
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (curLevel > userData.level) {
+    // Level up!
+    userData.level = curLevel;
+    message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
+  }
+
+  if (message.content.startsWith(prefix + "level")) {
+    message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  });
+
 });
 
 
