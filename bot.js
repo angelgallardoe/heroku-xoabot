2516690277631ce71b181f2638d9737c0c38d99d
angelgallardoe <43081306+angelgallardoe@users.noreply.github.com>
2018-09-28 11:12:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,28 @@ client.on('guildMemberAdd', member => {
   channel.send("Welcome to the server, ${member}");
 });
 
+// PING CHECK//
+client.on("message", (message)){ // EventEmitter
+	if(message.content == "!ping"){ // Check if message is "!ping"
+			message.channel.send("Pinging ...") // Placeholder for pinging ... 
+			.then((msg) => { // Resolve promise
+				msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
+			});
+		}
+}
+
+// M E N S A J E S //
+client.on("message", (message) => {
+  if (message.content == ("ping")) {
+    message.channel.send("pong!");
+  }
+  else if (message.content == "!ping") {
+    	message.reply("Pong!");
+  	} 
+    else if (message.content == "!cigarro") {
+        message.reply("No tengo gil conchetumare");
+     }  
+});
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login('NDkzOTY2MTY1Njg1NTY3NDk0.Dosukg.zXRcfBJRL1j4QBrlZ_UhoP2ICWQ');
