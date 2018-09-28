03021859a59a407822a1be3a2c7396b2622430be
angelgallardoe <43081306+angelgallardoe@users.noreply.github.com>
2018-09-28 10:47:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,20 @@
-// Import the discord.js module //
+/**
+ * A bot that welcomes new guild members when they join
+ */
+
+// Import the discord.js module
 const Discord = require('discord.js');
 
-// Create an instance of a Discord client //
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
-// THIS  MUST  BE  THIS  WAY //
-client.login(process.env.bot_token);
-
-// The ready event is vital, it means that only _after_ this will your bot start reacting to information received from Discord //
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-console.log('I am ready!');
+  console.log('I am ready!');
 });
-
-bot.on("message", function(message) {
-    var channel = bot.channels.find("name", "general");
-    channel.sendMessage("Hello Owner Just Restarted Me!");
-
-    var rule = new schedule.RecurrenceRule();
-    rule.minute = 0;
-    rule.hour = [14, 19, 20];
-
-    var j = schedule.scheduleJob(rule, function() {
-        bot.channels.get("id", channel).sendMessage("Testing");
-    })
-
-    console.log("Bot is ready.");
-});
-
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
@@ -35,24 +23,11 @@ client.on('guildMemberAdd', member => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send("Welcome to the server, ${member}");
+  channel.send(`Welcome to the server, ${member}`);
 });
 
-// M E N S A J E S //
-client.on("message", (message)){ // EventEmitter
-	if(message.content == "!ping"){ // Check if message is "!ping"
-			message.channel.send("Pinging ...") // Placeholder for pinging ... 
-			.then((msg) => { // Resolve promise
-				msg.edit("Ping: " + (Date.now() - msg.createdTimestamp)) // Edits message with current timestamp minus timestamp of message
-			});
-		}
-}
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login('NDkzOTY2MTY1Njg1NTY3NDk0.Dosukg.zXRcfBJRL1j4QBrlZ_UhoP2ICWQ');
 
-else if (message.content == "!ping") {
-    	message.reply("Pong!");
-  	} 
-        
-else if (message.content == "!cigarro") {
-        message.reply("No tengo gil conchetumare");
-     }  
-
+// THIS  MUST  BE  THIS  WAY //
+client.login(process.env.bot_token);
